40be83d77cffc8d2b1dc185e10fa12259851abf1
ztmills <hacker.6284@gmail.com>
2019-12-31 17:10:16 -0700

    Started GUI app

--- gui.py
+++ gui.py
@@ -1,7 +1,17 @@
 from tkinter import *
+import tkinter.ttk as ttk
+
 
 window = Tk()
 
 window.title("Welcome to the Movie Sort app")
+lbl = Label(window, text="Movie Sort", font=("Arial Bold", 20))
+lbl.grid(column=0, row=0)
+
+window.geometry('450x300')
+
+btn = Button(window, text="This One", bg="orange", fg="black",
+             width=70, height=105)
+btn.grid(column=0, row=1)
 
 window.mainloop()
